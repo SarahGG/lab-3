967975a23051deb49b6d9dfefd0c37e90d579320
SarahGG <guarino.sgg@gmail.com>
2017-04-12 22:43:00 -0400

    finished v1.0 of Lab Three

--- src/LabThree.java
+++ src/LabThree.java
@@ -2,28 +2,51 @@ import java.util.Scanner;
 import java.lang.*;
 
 /**
- * Created by Sarah Guarino on 4/12/2017.
+ * Displays a number of powers for
+ * each integer starting from 1 to
+ * the integer indicated by the
+ * user
+ *
+ * @author Sarah Guarino
+ * @version 1.0
  */
 public class LabThree {
-    public static Scanner scnr = new Scanner(System.in);
+    private static Scanner scnr = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int myNumber = -1;
+        int userInput = -1;
+        int baseNum;
+        int[][] myPowerArray;
         boolean doAgain = false;
-        boolean correctInput = false;
-        char myAnswer = 'z';
+        boolean correctInput;
+        char startOver = 'z';
+
 
         do {
             System.out.print("Please enter an integer: ");
-            myNumber = scnr.nextInt();
-            System.out.println(myNumber); // FIXME: delete
+            userInput = scnr.nextInt();
+            System.out.println();
 
-            System.out.println("Do you want to go again?: ");
+            System.out.println("Number\t\tSquared\t\tCubed");
+            System.out.println("======\t\t======\t\t======");
+
+            myPowerArray = new int[userInput][3];
+
+            for(int i = 0; i < userInput; i++) {
+                baseNum = i + 1;
+                myPowerArray[i][0] = baseNum;
+                myPowerArray[i][1] = (int)Math.pow((double)baseNum, 2.0);
+                myPowerArray[i][2] = (int)Math.pow((double)baseNum, 3.0);
+                System.out.println(myPowerArray[i][0] + "\t\t\t" + myPowerArray[i][1] + "\t\t\t" + myPowerArray[i][2]);
+            }
+
+            System.out.println();
+            System.out.print("Do you want to go again?: ");
             do {
                 // collects input the first character (a-z regardless of case), and switches to lowercase
-                myAnswer = Character.toLowerCase(scnr.next("[a-zA-Z]").charAt(0));
+                startOver = Character.toLowerCase(scnr.next("[a-zA-Z]").charAt(0));
 
-                switch (myAnswer) {
+                switch (startOver) {
                     case 'y':
                         correctInput = true;
                         doAgain = true;
@@ -36,8 +59,9 @@ public class LabThree {
 
                         break;
                     case 'z':
-                        System.out.println("Something went wrong with your switch!"); // FIXME: exception
+                        System.out.println("Something went wrong with your switch!");
                         correctInput = false;
+                        break;
 
                     default:
                         System.out.println("Incorrect input, please try again!");
